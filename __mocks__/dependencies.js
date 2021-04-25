08d43717e4866d3d0bcf0cdf90af2b37f6238f04
James Robinson <jrobind@outlook.com>
2021-04-25 18:43:12 +0100

    add test mocks

---
+++ __mocks__/dependencies.js
@@ -0,0 +1,14 @@
+export default {
+  name: 'test',
+  homepage: 'https://someurl.com',
+  dependencies: {
+    'node-fetch': '^2.6.1',
+  },
+  devDependencies: {
+    '@babel/cli': '^7.13.14',
+    '@babel/core': '^7.13.15',
+    husky: '^6.0.0',
+    jest: '^26.6.3',
+    typescript: '^4.2.4',
+  },
+};
---
+++ __mocks__/trimmed-api-data.js
@@ -0,0 +1,19 @@
+export default JSON.stringify([
+  {
+    name: 'testscript',
+    popularity: {
+      communityInterest: 161,
+      downloadsCount: 2345,
+      downloadsAcceleration: 1345.32,
+      dependentsCount: 1823,
+    },
+    score: {
+      final: 0.43437456068732777,
+      detail: {
+        quality: 0.2966542073336534,
+        popularity: 0.6534618052016146,
+        maintenance: 0.3333333333333333,
+      },
+    },
+  },
+]);
