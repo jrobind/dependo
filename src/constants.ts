1b98fc7d2bb9ecf1308706416fca81a4ec9f0464
James Robinson <jrobind@outlook.com>
2021-05-21 13:00:30 +0100

    add interfaces and constants files

---
+++ src/constants.ts
@@ -0,0 +1,5 @@
+export const API_URL = 'https://api.npms.io/v2/package/';
+export const FILE_MATCH = 'package.json';
+export const CURRENT_DIRECTORY = process.cwd();
+export const STYLES_PATH = `${CURRENT_DIRECTORY}/src/styles/style.css`;
+export const ETA_TEMPLATES = `${CURRENT_DIRECTORY}/src/templates/template.eta`;
---
+++ src/interfaces.ts
@@ -0,0 +1,20 @@
+import { DependencyType } from './lib';
+
+export interface PackageInformation {
+  name: string;
+  homepage: string;
+  githubStars: number;
+  popularity: number;
+  score: number;
+  type: DependencyType.DEPENDENCY | DependencyType.DEV_DEPENDENCY;
+}
+
+export interface PackageMeta {
+  name: string;
+  type: string;
+}
+
+export interface TemplateDependecyData {
+  dependencies: PackageInformation[];
+  devDependencies: PackageInformation[];
+}
