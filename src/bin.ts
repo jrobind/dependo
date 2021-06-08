943172d67119591ab79ea1412f9da6a1e985a94b
James Robinson <jrobind@outlook.com>
2021-06-08 10:08:34 +0100

    add commander cli framework

--- src/bin.ts
+++ src/bin.ts
@@ -1,12 +1,26 @@
 #!/usr/bin/env ts-node
 
-import { reformatFileUrl } from './utils';
+import { Command } from 'commander';
+
+import { extractRepoFileData } from './utils';
 import { generateReport } from './lib';
+import * as pack from '../package.json';
 
-const args = process.argv.slice(2);
+const program = new Command();
 
-if (args.length) {
-  generateReport(reformatFileUrl(args[0]));
+program
+  .version(pack.version)
+  .option('-f, --file [path]', 'a GitHub package.json file url')
+  .option(
+    '-o, --out [out]',
+    'the output folder for the generated dependency map',
+  )
+  .parse(process.argv);
+
+const options = program.opts();
+
+if (options.file) {
+  generateReport(extractRepoFileData(options.file));
 } else {
   generateReport();
 }
